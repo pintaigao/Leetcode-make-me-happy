cbfeb1d7712690f0e0d7f382a6aded487bc0c6d2
hptg <hptg1994@gmail.com>
2017-09-10 12:01:47 -0400

    Finished to 485

---
+++ 485.max-consecutive-ones.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxHere = 0, max = 0;
+        for (int n : nums)
+            max = Math.max(max, maxHere = n == 0 ? 0 : maxHere + 1);
+        return max; 
+    } 
+}
