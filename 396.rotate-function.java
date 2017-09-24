087720832aeb664d6e071d191a478a831023eac1
Pintaigao He <hptg1994@gmail.com>
2017-09-24 18:31:29 -0400

    Finish 396

---
+++ 396.rotate-function.java
@@ -0,0 +1,20 @@
+public class Solution {
+
+	public int maxRotateFunction(int[] A) {
+		int n = A.length;
+		int sum = 0;
+		int candidate = 0;
+
+		for (int i = 0; i < n; i++) {
+			sum += A[i];
+			candidate += A[i] * i;
+		}
+		int best = candidate;
+
+		for (int i = n - 1; i > 0; i--) {
+			candidate = candidate + sum - A[i] * n;
+			best = Math.max(best, candidate);
+		}
+		return best;
+	}
+}
