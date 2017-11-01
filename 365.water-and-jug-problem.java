92cc67895303b6053432f8b19c433503cabeedaf
hptg1994 <hptg1994@gmail.com>
2017-10-31 22:31:48 -0400

    ???

---
+++ 365.water-and-jug-problem.java
@@ -0,0 +1,10 @@
+class Solution {
+	public boolean canMeasureWater(int x, int y, int z) {
+		return x + y == z || (x + y > z ) && z % gcd(x,y) == 0;
+	}
+	
+	private int gcd(int a,int b){
+		return b==0? a: gcd(b,a%b);
+	}
+
+}
