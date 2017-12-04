4df58630c56a8bbdb5b2180c38cb40afaf45e072
hptg1994 <hptg1994@gmail.com>
2017-12-04 18:20:49 -0500

    Unknown finished

--- 713.subarray-product-less-than-k.java
+++ 713.subarray-product-less-than-k.java
@@ -1,5 +1,16 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        
+        int result = 0;
+        for(int i = 0;i<nums.length;i++){
+            int product = 1;
+            for(int j = i;j<nums.length;j++){
+                product *= nums[j];
+                if(product < k)
+                    result ++;
+                else
+                    break;
+            }
+        }
+        return result;
     }
 }
