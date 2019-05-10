016af31c453090ff61951325da7747a0252b933f
hptg1994 <hptg1994@gmail.com>
2019-05-10 10:48:27 -0500

    add ?

--- Easy/438.find-all-anagrams-in-a-string.js
+++ Easy/438.find-all-anagrams-in-a-string.js
@@ -11,17 +11,35 @@ var findAnagrams = function (s, p) {
     map[p[i].charCodeAt() - 97]++;
   }
 
+
   let j = 0;
   const r = [];
+
   for (let i = 0; i < s.length; i++) {
     const c = s[i].charCodeAt() - 97;
     map[c]--;
+
+    if (map[c] < 0) {
+      console.log("map[c] is < 0 and c is " + c);
+    }
+
     while (map[c] < 0) {
+      console.log("Before j++ j is: " + j);
       const c2 = s[j++].charCodeAt() - 97;
       map[c2]++;
+      console.log("Now the new map will be:");
+      console.log(map);
     }
-    if (i - j + 1 === p.length) r.push(j);
+
+    if (i - j + 1 === p.length) {
+      console.log("J will be push and j is:" + j);
+      r.push(j);
+    };
+    console.log();
   }
 
   return r;
 };
+
+
+findAnagrams("cbaebabacd", "abc");
