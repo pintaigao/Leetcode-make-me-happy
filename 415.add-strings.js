52e8798f83fdfd57b109dea2fa06bf9ca5cde530
pintaigao <pintaigao@gmail.com>
2021-01-08 14:15:39 -0500

    keep doing

---
+++ 415.add-strings.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=415 lang=javascript
+ *
+ * [415] Add Strings
+ */
+
+// @lc code=start
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    
+};
+// @lc code=end
+
