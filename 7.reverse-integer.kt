2d5791872f181e7deb5c3c8894c6d4c7245745da
pintaigao <pintaigao@gmail.com>
2021-01-08 23:45:36 -0500

    keep the process

---
+++ 7.reverse-integer.kt
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=7 lang=kotlin
+ *
+ * [7] Reverse Integer
+ */
+
+// @lc code=start
+class Solution {
+    fun reverse(x: Int): Int {
+        
+    }
+}
+// @lc code=end
+
