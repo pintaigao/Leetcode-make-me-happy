4dc21ef9bced91010a330d1a1eb47e27a3153c17
pintaigao <pintaigao@gmail.com>
2021-01-29 17:47:05 -0600

    keep the process

--- 209.minimum-size-subarray-sum.js
+++ 209.minimum-size-subarray-sum.js
@@ -8,20 +8,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minSubArrayLen = function(s, nums) {
+var minSubArrayLen = function (s, nums) {
   let left = 0;
   let sum = 0;
-  let min = Infinity;
-  
-  for (let right = 0; right < nums.length; right++) {
-      sum += nums[right];
-      while (sum >= s) {
-          min = Math.min(min, right + 1 - left);
-          sum -= nums[left];
-          left++;
-      }
-  }
-  
-  return (min === Infinity) ? 0 : min;
-};
+  let min = nums.length + 1;
 
+  for (let right = 0; right < nums.length; right++) {
+    sum += nums[right];
+    while (sum >= s) {
+      min = Math.min(min, right - left + 1);
+      sum -= nums[left];
+      left += 1;
+    }
+  }
+
+  return min === nums.length + 1 ? 0 : min;
+};
