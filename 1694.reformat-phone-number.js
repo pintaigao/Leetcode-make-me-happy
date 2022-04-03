8b44ef98b1ec100e35ad69c12dd3e038eff039f0
pintaigao <pintaigao@gmail.com>
2022-04-03 12:52:36 -0500

    add 1694

---
+++ 1694.reformat-phone-number.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=1694 lang=javascript
+ *
+ * [1694] Reformat Phone Number
+ */
+
+// @lc code=start
+/**
+ * @param {string} number
+ * @return {string}
+ */
+var reformatNumber = function (number) {};
+// @lc code=end
