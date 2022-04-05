c1d8215d38b59ffcd87629ae89435e127859e908
pintaigao <pintaigao@gmail.com>
2022-04-05 00:31:39 -0500

    Add 1423.Maximum Points You Can Obtain from Cards

--- 1047.remove-all-adjacent-duplicates-in-string.js
+++ 1047.remove-all-adjacent-duplicates-in-string.js
@@ -15,6 +15,7 @@ var removeDuplicates = function (s) {
   for (const ch of s) {
     if (stack.length && stack[stack.length - 1] === ch) {
       stack.pop();
+      continue;
     } else {
       stack.push(ch);
     }
