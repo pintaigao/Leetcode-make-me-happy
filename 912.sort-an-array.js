165b4911eb9e039692b8c9cee7a9e96278d76d82
Pintaigao He <pintaigao@gmail.com>
2022-04-16 09:57:18 -0500

    04/15/2022 (#11)
    * Add 934.Shortest Bridge
    
    * Add 918.Maximum Sum Circular Subarray
    
    * Add 912.Sort an Array
    
    * Add 912.Sort an Array
    
    * Add 905.Sort Array By Parity
    
    * 896.Monotonic Array

---
+++ 912.sort-an-array.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=912 lang=javascript
+ *
+ * [912] Sort an Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function (nums) {
+  return nums.sort((a, b) => a - b);
+};
+// @lc code=end
