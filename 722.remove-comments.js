9b9a72d854bea4327e5f04ff615be0858960752b
pintaigao <pintaigao@gmail.com>
2022-04-25 20:52:33 -0500

    Add 723.Candy Crush

---
+++ 722.remove-comments.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=722 lang=javascript
+ *
+ * [722] Remove Comments
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} source
+ * @return {string[]}
+ */
+var removeComments = function(source) {
+    
+};
+// @lc code=end
+
