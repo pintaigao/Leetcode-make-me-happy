eaac7e4ac461c9a3db53cee43f558960bf1874ff
pintaigao <pintaigao@gmail.com>
2022-06-06 22:19:39 -0500

    Add 306.Additive Number

---
+++ 306.additive-number.js
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=306 lang=javascript
+ *
+ * [306] Additive Number
+ */
+
+// @lc code=start
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var isAdditiveNumber = function (num) {
+  let isValid = function (i, j, num) {
+    if (num[0] == "0" && i > 1) return false;
+    if (num[i] == "0" && j > 1) return false;
+    let sum = 0;
+    let x1 = parseInt(num.substring(0, i));
+    let x2 = parseInt(num.substring(i, i + j));
+    for (let start = i + j; start != num.length; start += sum.length) {
+      x2 = x2 + x1;
+      x1 = x2 - x1;
+      sum = x2.toString();
+      if (!num.startsWith(sum, start)) return false;
+    }
+    return true;
+  };
+
+  let n = num.length;
+  // i表示substring从0到i
+  // for (let i = 1; i <= parseInt(n / 2); ++i) {
+  //   // j表示substring从i到i+j,即后面接数的长度从1开始
+  //   for (let j = 1; Math.max(j, i) <= n - i - j; ++j) {
+  //     if (isValid(i, j, num)) return true;
+  //   }
+  // }
+
+  for (let i = 1; i <= n - 1; ++i) {
+    // j表示substring从i到i+j,即后面接数的长度从1开始
+    for (let j = 0; j <= i; ++j) {
+      if (isValid(i, j, num)) return true;
+    }
+  }
+  return false;
+};
+// @lc code=end
