65a2cfb7b02843b32ecf2f0212d59ebf680ec217
pintaigao <pintaigao@gmail.com>
2022-06-14 13:08:19 -0500

    Add 247.Strobogrammatic Number II

---
+++ 246.strobogrammatic-number.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=246 lang=javascript
+ *
+ * [246] Strobogrammatic Number
+ */
+
+// @lc code=start
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var isStrobogrammatic = function(num) {
+    
+};
+// @lc code=end
+
