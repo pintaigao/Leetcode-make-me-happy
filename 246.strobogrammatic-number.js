4ca60c50a6d082ad8aa9142f4d3174c7ab10d2cc
pintaigao <pintaigao@gmail.com>
2022-06-14 13:20:12 -0500

    Add 246.Strobogrammatic Number

--- 246.strobogrammatic-number.js
+++ 246.strobogrammatic-number.js
@@ -9,8 +9,24 @@
  * @param {string} num
  * @return {boolean}
  */
-var isStrobogrammatic = function(num) {
-    
+var isStrobogrammatic = function (num) {
+  // Note that using a String here and appending to it would be
+  // poor programming practice.
+  let rotatedStringBuilder = "";
+  // Remember that we want to loop backwards through the string
+  for (let i = num.length - 1; i >= 0; i--) {
+    let c = num[i];
+    if (c == "0" || c == "1" || c == "8") {
+      rotatedStringBuilder += c;
+    } else if (c == "6") {
+      rotatedStringBuilder += "9";
+    } else if (c == "9") {
+      rotatedStringBuilder += "6";
+    } else {
+      // This must be an invalid digit.
+      return false;
+    }
+  }
+  return num === rotatedStringBuilder;
 };
 // @lc code=end
-
