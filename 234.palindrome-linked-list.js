f2d584c58601bcc6756989b0e56b2325c364f7a8
pintaigao <pintaigao@gmail.com>
2022-06-16 15:54:36 -0500

    Add 231.Power of Two

---
+++ 234.palindrome-linked-list.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=234 lang=javascript
+ *
+ * [234] Palindrome Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    
+};
+// @lc code=end
+
