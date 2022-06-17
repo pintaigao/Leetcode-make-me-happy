5b1bb6d8c3bd5adfbc0a3bec208e587859db62e9
pintaigao <pintaigao@gmail.com>
2022-06-17 16:20:03 -0500

    228.Summary Ranges

---
+++ 228.summary-ranges.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=228 lang=javascript
+ *
+ * [228] Summary Ranges
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function (nums) {
+  if (!nums.length) {
+    return [];
+  }
+  let pointer = 0;
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    pointer = i;
+    while (i !== nums.length && pointer !== nums.length - 1 && nums[pointer + 1] === nums[pointer] + 1) {
+      pointer += 1;
+    }
+    if (pointer === i) {
+      result.push(nums[i] + "");
+    } else {
+      let string = nums[i] + "->" + nums[pointer];
+      result.push(string);
+    }
+
+    i = pointer;
+  }
+  return result;
+};
+// @lc code=end
