c81213667be1e30e6148fe7349d4613457be2447
pintaigao <pintaigao@gmail.com>
2022-06-20 15:05:31 -0500

    Add 215.Kth Largest Element in an Array

---
+++ 215.kth-largest-element-in-an-array.js
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=215 lang=javascript
+ *
+ * [215] Kth Largest Element in an Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+/* Quick Select */
+var findKthLargest = function (nums, k) {
+  function swap(nums, i, j) {
+    let temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
+
+  function quickSelect(nums, k, startIndex, endIndex) {
+    let pivot = startIndex;
+    for (let j = startIndex; j < endIndex; j++) {
+      if (nums[j] <= nums[endIndex]) {
+        swap(nums, pivot++, j);
+      }
+    }
+    swap(nums, pivot, endIndex);
+    // count the nums that are > pivot from high
+    let count = endIndex - pivot + 1;
+    // pivot is the one!
+    if (count == k) return nums[pivot];
+    // pivot is too small, so it must be on the right
+    if (count > k) return quickSelect(nums, k, pivot + 1, endIndex);
+    else {
+      // pivot is too big, so it must be on the left
+      return quickSelect(nums, k - count, startIndex, pivot - 1);
+    }
+  }
+
+  return quickSelect(nums, k, 0, nums.length - 1);
+};
+// @lc code=end
