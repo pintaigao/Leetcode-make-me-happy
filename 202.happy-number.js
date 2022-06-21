75cdb1aa5f0f21e153f5f3bc10bd68ee0301223d
pintaigao <pintaigao@gmail.com>
2022-06-21 18:29:26 -0500

    Add 202.Happy Number

---
+++ 202.happy-number.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=202 lang=javascript
+ *
+ * [202] Happy Number
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function (n) {
+  let set = new Set();
+  while (true) {
+    let sum = 0;
+
+    while (n !== 0) {
+      let number = n % 10;
+      sum += number * number;
+      n = parseInt(n / 10);
+    }
+
+    if (sum === 1) {
+      return true;
+    }
+    // 如果set又遇见一遍这个sum，就是死循环了
+    if (set.has(sum)) {
+      return false;
+    }
+
+    n = sum;
+    set.add(n);
+  }
+};
+// @lc code=end
