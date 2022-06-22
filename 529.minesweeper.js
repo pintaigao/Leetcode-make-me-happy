7d656cba88d4892067b954bd7e079feb2a5f5a2a
pintaigao <pintaigao@gmail.com>
2022-06-21 20:39:39 -0500

    Add 200.Number of Islands

---
+++ 529.minesweeper.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=529 lang=javascript
+ *
+ * [529] Minesweeper
+ */
+
+// @lc code=start
+/**
+ * @param {character[][]} board
+ * @param {number[]} click
+ * @return {character[][]}
+ */
+var updateBoard = function(board, click) {
+    
+};
+// @lc code=end
+
