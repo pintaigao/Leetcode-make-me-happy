7a50179a144c59dd12524bfa3dba5364ae54d59a
pintaigao <pintaigao@gmail.com>
2022-07-01 01:25:02 -0500

    Add 1762.Buildings With an Ocean View

---
+++ 1762.buildings-with-an-ocean-view.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} heights
+ * @return {number[]}
+ */
+var findBuildings = function (heights) {
+  let list = [];
+  let max = 0;
+  for (let i = heights.length - 1; i >= 0; i--) {
+    if (heights[i] > max) {
+      list.unshift(i);
+      max = heights[i];
+    }
+  }
+  return ans;
+};
