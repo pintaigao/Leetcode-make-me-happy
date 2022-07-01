3c2599acd9a6a61a18b62e6cf269678891cad760
pintaigao <pintaigao@gmail.com>
2022-07-01 17:05:56 -0500

    Add 5.Longest Palindromic Substring

--- 5.longest-palindromic-substring.js
+++ 5.longest-palindromic-substring.js
@@ -9,8 +9,25 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function(s) {
-    
+var longestPalindrome = function (str) {
+  function helper(start, end) {
+    while (start >= 0 && end < str.length && str[start] == str[end]) {
+      start--;
+      end++;
+    }
+    if (maxLen < end - start - 1) {
+      lo = start + 1;
+      maxLen = end - start - 1;
+    }
+  }
+
+  let [lo, maxLen] = [0, 0];
+  if (str.length < 2) return str;
+  for (let i = 0; i < str.length - 1; i++) {
+    helper(i, i);
+    helper(i, i + 1);
+  }
+
+  return str.substring(lo, lo + maxLen);
 };
 // @lc code=end
-
