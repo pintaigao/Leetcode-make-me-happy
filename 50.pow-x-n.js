039dca4e693778ecd87a920e065f450a685159ee
pintaigao <pintaigao@gmail.com>
2022-07-06 17:51:57 -0500

    Add 50.Pow(x, n)

--- 50.pow-x-n.js
+++ 50.pow-x-n.js
@@ -10,8 +10,16 @@
  * @param {number} n
  * @return {number}
  */
-var myPow = function(x, n) {
-    
+var myPow = function (x, n) {
+  if (n === 0) {
+    return 1;
+  }
+  if (n < 0) {
+    return 1 / myPow(x, -n);
+  }
+  if (n % 2) {
+    return x * myPow(x, n - 1);
+  }
+  return myPow(x * x, n / 2);
 };
 // @lc code=end
-
