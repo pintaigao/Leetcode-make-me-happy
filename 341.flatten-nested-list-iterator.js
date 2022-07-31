acf2f535df9abecf0de30f6badeb113c57ca894e
pintaigao <pintaigao@gmail.com>
2022-07-31 17:13:36 -0500

    Add 341.Flatten Nested List Iterator

---
+++ 341.flatten-nested-list-iterator.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=341 lang=javascript
+ *
+ * [341] Flatten Nested List Iterator
+ */
+
+// @lc code=start
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * function NestedInteger() {
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     @return {boolean}
+ *     this.isInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     @return {integer}
+ *     this.getInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     Return null if this NestedInteger holds a single integer
+ *     @return {NestedInteger[]}
+ *     this.getList = function() {
+ *         ...
+ *     };
+ * };
+ */
+/**
+ * @constructor
+ * @param {NestedInteger[]} nestedList
+ */
+var NestedIterator = function (nestedList) {
+  this.result = [];
+  let dfs = (nestedList) => {
+    for (let n of nestedList) {
+      if (n.isInteger()) {
+        this.result.push(n.getInteger());
+      } else {
+        dfs(n.getList());
+      }
+    }
+  };
+  dfs(nestedList);
+};
+
+/**
+ * @this NestedIterator
+ * @returns {boolean}
+ */
+NestedIterator.prototype.hasNext = function () {
+  return this.result.length;
+};
+
+/**
+ * @this NestedIterator
+ * @returns {integer}
+ */
+NestedIterator.prototype.next = function () {
+  return this.result.shift();
+};
+
+/**
+ * Your NestedIterator will be called like this:
+ * var i = new NestedIterator(nestedList), a = [];
+ * while (i.hasNext()) a.push(i.next());
+ */
+// @lc code=end
