413eabc0d82fe554fd5775cc0140dbef590c3b8b
pintaigao <pintaigao@gmail.com>
2022-09-08 17:29:42 -0500

    Add 287.Find the Duplicate Number

---
+++ 287.find-the-duplicate-number.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=287 lang=javascript
+ *
+ * [287] Find the Duplicate Number
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function (nums) {
+  for (let num of nums) {
+    if (nums[Math.abs(num) - 1] < 0) {
+      return Math.abs(num);
+    }
+    nums[Math.abs(num) - 1] *= -1;
+  }
+};
+// @lc code=end
