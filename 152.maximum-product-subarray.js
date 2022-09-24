3d4d8b7c896fc2f66075bc29271634fb613be079
pintaigao <pintaigao@gmail.com>
2022-09-24 17:16:43 -0500

    Add 152.Maximum Product Subarray

--- 152.maximum-product-subarray.js
+++ 152.maximum-product-subarray.js
@@ -9,19 +9,24 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+/* Solution 1: 指针*/
 var maxProduct = function (nums) {
-  let [max, imax, imin] = [Number.MIN_VALUE, 1, 1];
+  let [max, imax, imin] = [Number.NEGATIVE_INFINITY, 1, 1];
   for (let i = 0; i < nums.length; i++) {
+    // 如果遇到负的，把正的最大值赋予给负的，以准备再遇到负数的时候重新变回正的
     if (nums[i] < 0) {
       let tmp = imax;
       imax = imin;
       imin = tmp;
     }
     imax = Math.max(imax * nums[i], nums[i]);
+    // 保留最小的数，即最大的负数
     imin = Math.min(imin * nums[i], nums[i]);
 
     max = Math.max(max, imax);
   }
   return max;
 };
+
 // @lc code=end
