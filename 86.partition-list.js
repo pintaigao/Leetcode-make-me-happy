3165e46f2ebda054afa5900430a56932a6e5c5fb
pintaigao <pintaigao@gmail.com>
2022-12-07 21:39:39 -0600

    Add 109.Convert Sorted List to Binary Search Tree

---
+++ 86.partition-list.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=86 lang=javascript
+ *
+ * [86] Partition List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+    
+};
+// @lc code=end
+
