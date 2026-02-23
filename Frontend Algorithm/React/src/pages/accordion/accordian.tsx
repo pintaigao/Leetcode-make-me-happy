46e06e812770b7d1df8f82cdd42918309776740a
Pintaigao He <pintaigao@gmail.com>
2026-02-23 12:14:14 -0600

    wikp

--- Frontend Algorithm/React/src/pages/accordion/accordian.tsx
+++ Frontend Algorithm/React/src/pages/accordion/accordian.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useId, useState } from 'react';
 import './accordian.scss';
 
 let sections = [
@@ -22,40 +22,87 @@ let sections = [
   },
 ]
 
+function getAccordionHeaderId(accordionId, value) {
+  return accordionId + '-header-' + value;
+}
+
+function getAccordionPanelId(accordionId, value) {
+  return accordionId + '-panel-' + value;
+}
+
 export default function Accordion(props) {
+  const accordionId = useId();
   const [openSections, setOpenSections] = useState(
     new Set(),
   );
   
-  function handleOpen(value) {
-    const newOpenSections = new Set(openSections);
-    if (newOpenSections.has(value)) {
-      newOpenSections.delete(value);
-    } else {
-      newOpenSections.add(value);
+  function focusOnSection(index) {
+    document.getElementById(getAccordionHeaderId(accordionId, sections[index].value)).focus();
+  }
+  
+  function handleOnKeyDown(event) {
+    const activeItemValue =
+      document.activeElement.getAttribute('data-accordion-value',);
+    
+    // Only respond to these interactions if
+    // an accordion title is in focus.
+    if (activeItemValue == null) return;
+    
+    switch (event.code) {
+      case 'ArrowUp': {
+        const index = sections.findIndex(({ value: itemValue }) => itemValue === activeItemValue);
+        focusOnSection((index - 1 + sections.length) % sections.length);
+        break;
+      }
+      case 'ArrowDown': {
+        const index = sections.findIndex(({ value: itemValue }) => itemValue === activeItemValue);
+        focusOnSection((index + 1) % sections.length);
+        break;
+      }
+      case 'Home': {
+        focusOnSection(0);
+        break;
+      }
+      case 'End': {
+        focusOnSection(sections.length - 1);
+        break;
+      }
+      default:
+        break;
     }
+  }
+  
+  function handleOpenSection(value) {
+    const newOpenSections = new Set(openSections);
+    newOpenSections.has(value) ? newOpenSections.delete(value) : newOpenSections.add(value);
     setOpenSections(newOpenSections);
   }
   
   return (
-    <div className="accordion">
-      {sections.map(({value, title, contents}) => {
-        const isExpanded = openSections.has(value);
+    <div
+      className="accordion"
+      onKeyDown={handleOnKeyDown}>
+      {sections.map(({ value, title, contents }) => {
+        const isExpanded = openSections.has(value), headerId = getAccordionHeaderId( accordionId, value), panelId = getAccordionPanelId(accordionId, value);
         
         return (
           <div className="accordion-item" key={value}>
             <button
+              aria-controls={panelId}
+              aria-expanded={isExpanded}
+              id={headerId}
               className="accordion-item-title"
               type="button"
-              onClick={() => handleOpen(value)}>
-              {title}
-              <span
-                aria-hidden={true}
-                className={['accordion-icon', isExpanded && 'accordion-icon--rotated'].filter(Boolean).join(' ')}
-              />
+              data-accordion-value={value}
+              onClick={() => handleOpenSection(value)}>
+              {title}{' '}
+              <span aria-hidden={true} className={['accordion-icon', isExpanded && 'accordion-icon--rotated'].filter(Boolean).join(' ')} />
             </button>
             <div
+              aria-labelledby={headerId}
+              role="region"
               className="accordion-item-contents"
+              id={panelId}
               hidden={!isExpanded}>
               {contents}
             </div>
