d5fdef501b72cf870225c8489a3401b6511fff16
Pintaigao He <pintaigao@gmail.com>
2026-03-26 13:55:31 -0500

    wip

--- 2026/Easy/26.删除有序数组中的重复项.js
+++ 2026/Easy/26.删除有序数组中的重复项.js
@@ -9,11 +9,34 @@ var removeDuplicates = function (nums) {
     // 更新 slow 指针到下一位，并把 nums[fast] 的值赋给 nums[slow] （这样子nums[slow]不和nums[slow-1]相等）
     if (nums[fast] !== nums[slow]) {
       slow++;
-      // 维护 nums[0..slow] 无重复
+      // 维护 nums[0..slow] 无重复!!!
       nums[slow] = nums[fast];
     }
+
+    // fast 基本炒作，每一次 fast+=1
     fast++;
   }
   // 数组长度为索引 + 1
   return slow + 1;
 };
+
+// 练习
+var removeDuplicates2 = function (nums) {
+  // I have a current pointer and a next pointer, current pointer is the last unique element, next pointer is the next element to compare with current pointer, if they are equal, next pointer moves forward, if they are not equal, current pointer moves forward and next pointer's value is assigned to current pointer's value
+  let current = 0, next = 0;
+  while (next < nums.length) {
+    if (nums[next] === nums[current]) {
+      // If they are equal, next pointer moves forward, find the one that is not equal to current pointer
+      next++;
+    } else {
+      // If they are not equal, current pointer moves forward and next pointer's value is assigned to current pointer's value
+      // Why current pointer moves forward?  因为 current 维持着 nums[0..current] 无重复，所以 current 需要往前走一位，来维护这个无重复的状态
+      current += 1;
+      nums[current] = nums[next];
+    }
+  }
+
+  return nums
+};
+
+removeDuplicates2([0, 1, 1, 1, 2, 2, 3, 3, 4]);
--- 2026/Hard/42.trapping-rain-water.js
+++ 2026/Hard/42.trapping-rain-water.js
@@ -1,17 +1,5 @@
-/*
- * @lc app=leetcode id=42 lang=javascript
- *
- * [42] Trapping Rain Water
- */
-
-// @lc code=start
-/**
- * @param {number[]} height
- * @return {number}
- */
-
 /* 一：按行数求 */
-var trap = function (height) {
+var trap1 = function (height) {
   let sum = 0;
   //找到最大的高度，以便遍历。
   let max = 0;
@@ -43,7 +31,7 @@ var trap = function (height) {
 /* 按列求
  * 每一列的水，我们只需要关注当前列，以及左边最高的墙，右边最高的墙就够了。
  */
-let trap = function (height) {
+let trap2 = function (height) {
   let sum = 0;
   //最两端的列不用考虑，因为一定不会有水。所以下标从 1 到 length - 2
   for (let i = 1; i < height.length - 1; i++) {
@@ -74,7 +62,7 @@ let trap = function (height) {
  * 从上一个题解可以看出，对于每一列，我们求它左边最高的墙和右边最高的墙，都是重新遍历一遍所有高度，这里我们可以优化一下。
  * 即提前得知每个位置
  */
-let trap = function (height) {
+let trap3 = function (height) {
   let [sum, max_left, max_right] = [0, new Array(height.length).fill(0), new Array(height.length).fill(0)];
 
   // 对于i这个位置，每个位置的值为它左边最高的墙的高度
@@ -99,7 +87,7 @@ let trap = function (height) {
 };
 
 /* 四：双指针 */
-let trap = function (height) {
+let trap4 = function (height) {
   let sum = 0;
   let max_left = 0;
   let max_right = 0;
@@ -128,7 +116,7 @@ let trap = function (height) {
 };
 
 /* 五：Stack */
-let trap = function (height) {
+let trap5 = function (height) {
   let sum = 0;
   let stack = [];
   let current = 0;
@@ -151,3 +139,75 @@ let trap = function (height) {
   return sum;
 };
 // @lc code=end
+
+
+// 练习 1.暴力解法
+let trap6 = function (height) {
+  let maxHeight = Math.max(...height), h = 1, sum = 0;
+
+  while (h <= maxHeight) {
+    let currentIndex = 0, currentPosition = -1;
+    while (currentIndex < height.length) {
+      if (height[currentIndex] >= h && currentPosition >= 0) {
+        // 计算水量
+        sum = sum + currentIndex - currentPosition - 1;
+        currentPosition = currentIndex;
+      } else if (height[currentIndex] >= h) {
+        currentPosition = currentIndex;
+      }
+
+      currentIndex++;
+    }
+
+    h += 1;
+  }
+
+  return sum;
+};
+
+// 2.关注点不是考虑柱子有多高，而是这个位置能装多少水：暴力解法
+let trap7 = function (height) {
+  var n = height.length, res = 0;
+  for (var i = 0; i < n; i++) {
+    var l_max = 0, r_max = 0;
+    // 找右边最高的柱子
+    for (var j = i; j < n; j++)
+      r_max = Math.max(r_max, height[j]);
+    // 找左边最高的柱子
+    for (var j = i; j >= 0; j--)
+      l_max = Math.max(l_max, height[j]);
+    // 如果自己就是最高的话，
+    // l_max == r_max == height[i]
+    res += Math.min(l_max, r_max) - height[i];
+  }
+  return res;
+};
+
+// 3. 上面带备忘录的暴力解法
+let trap8 = function (height) {
+  let n = height.length;
+  let res = 0;
+  // 数组充当备忘录
+  let l_max = new Array(n).fill(0), r_max = new Array(n).fill(0);
+  l_max[0] = height[0], r_max[n - 1] = height[n - 1];
+  // 从左向右计算 l_max
+  for (let i = 1; i < n; i++)
+    l_max[i] = Math.max(height[i], l_max[i - 1]);
+  // 从右向左计算 r_max
+  for (let i = n - 2; i >= 0; i--)
+    r_max[i] = Math.max(height[i], r_max[i + 1]);
+  // 计算答案
+
+
+  for (let i = 1; i < n - 1; i++)
+    res += Math.min(l_max[i], r_max[i]) - height[i];
+
+  console.log(res);
+
+
+  return res;
+}
+
+
+// trap6([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]);
+trap8([4, 2, 0, 3, 2, 5]);
