c1f3be377297965ba55f1e6673dded935ca4d1b5
Pintaigao He <pintaigao@gmail.com>
2026-04-14 11:21:04 -0500

    wip

--- 863.all-nodes-distance-k-in-binary-tree.js
+++ 863.all-nodes-distance-k-in-binary-tree.js
@@ -18,8 +18,52 @@
  * @param {number} k
  * @return {number[]}
  */
-var distanceK = function(root, target, k) {
-    
+var distanceK = function (root, target, k) {
+  // 记录父节点：node.val -> parentNode
+  // 题目说了树中所有节点值都是唯一的，所以可以用 node.val 代表 TreeNode
+  // 开始从 target 节点施放 BFS 算法，找到距离为 k 的节点
+  // 记录离 target 的距离
+  let parent = {}, q = [target], visited = new Set([target.val]), dist = 0, res = [];
+
+  // 遍历所有节点，记录每个节点的父节点
+  function traverse(node, parentNode) {
+    if (node === null) return;
+    parent[node.val] = parentNode;
+    // 二叉树递归框架
+    traverse(node.left, node);
+    traverse(node.right, node);
+  }
+
+  traverse(root, null);
+
+  while (q.length > 0) {
+    let sz = q.length;
+    for (let i = 0; i < sz; i++) {
+      let cur = q.shift();
+      if (dist === k) {
+        // 找到距离起点 target 距离为 k 的节点
+        res.push(cur.val);
+      }
+      // 向父节点、左右子节点扩散
+      let parentNode = parent[cur.val];
+      if (parentNode !== null && !visited.has(parentNode.val)) {
+        visited.add(parentNode.val);
+        q.push(parentNode);
+      }
+      if (cur.left !== null && !visited.has(cur.left.val)) {
+        visited.add(cur.left.val);
+        q.push(cur.left);
+      }
+      if (cur.right !== null && !visited.has(cur.right.val)) {
+        visited.add(cur.right.val);
+        q.push(cur.right);
+      }
+    }
+    // 向外扩展一圈
+    dist++;
+  }
+
+  return res;
 };
 // @lc code=end
 
