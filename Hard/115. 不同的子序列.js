1ec4ae3227f797c45fb5e79dc28ef7ba5ebb1733
Pintaigao He <pintaigao@gmail.com>
2026-06-22 13:51:49 -0500

    wip

--- Hard/115. 不同的子序列.js
+++ Hard/115. 不同的子序列.js
@@ -1,8 +1,11 @@
+// DFS的方法
 var numDistinct = function (s, t) {
+
+  // memo 记录的是
   let memo = Array.from({ length: s.length }, () => Array(t.length).fill(-1)), m = s.length, n = t.length, res = 0;
   // 定义：该函数返回 s[i..] 中的子序列 t[j..] 的数量
-  // @visualize status(i, j)
   // i, j 分别是 s 和 t 的索引
+  // 因为 dp 传了两个参数（两个状态），所以 memo 可以是一个二维数组（代表我看到 i，j 这个时候的记录）
   var dp = function (i, j) {
     if (j == n) {
       // 子序列全部匹配完成
@@ -18,12 +21,14 @@ var numDistinct = function (s, t) {
     }
     // 状态转移方程
     if (s.charAt(i) == t.charAt(j)) {
-      // 可以选择匹配 s[i] 和 t[j]，也可以选择不匹配 s[i] 从而看看后面有没有
+      // 可以选择匹配 s[i] 和 t[j]（即看 s[i+1] 和 t[j+1]），也可以选择不匹配 s[i] 从而看看后面有没有 （即 t 这个位置保持不动，不选择当前 s[i]而是选择s[i+1]和 t[j]相匹配）
       res += dp(i + 1, j + 1) + dp(i + 1, j);
     } else {
+      // 如果不相等，只能选择s 中后一位和当前t[j]相匹配
       res += dp(i + 1, j);
     }
 
+    // i，j 这个情况下往后的情况我都计算过了，所以 1.memo[i][j] = res; 2. return res;
     memo[i][j] = res;
     return res;
   }
@@ -35,33 +40,55 @@ var numDistinct = function (s, t) {
 
 numDistinct("rabbbit", "rabbit");
 
-// DP 的方法
+// 另外一个 DFS with Memo 的方法
 var numDistinct = function (s, t) {
+  const memo = new Map();
+
+  function dfs(i, j) {
+    if (j === t.length) return 1;
+    if (i === s.length) return 0;
+
+    const key = `${i},${j}`;
+    if (memo.has(key)) return memo.get(key);
+
+    let result = 0;
+
+    if (s[i] === t[j]) {
+      result = dfs(i + 1, j + 1) + dfs(i + 1, j);
+    } else {
+      result = dfs(i + 1, j);
+    }
+
+    memo.set(key, result);
+    return result;
+  }
+
+  return dfs(0, 0);
+};
+
+// DP buttom up 的方法
+var numDistinct = function (s, t) {
+  // dp: 用 s 的前 i 个字符，组成 t 的前 j 个字符，有多少种方式
   const n = s.length, m = t.length, dp = Array.from({ length: s.length + 1 }, () => Array(t.length + 1).fill(0))
   for (let i = 0; i <= n; i++) {
+    // t 是空字符串时，任何 s 都有一种方式组成它：什么都不选。
     dp[i][0] = 1
   }
   for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= m; j++) {
-      if (s[i - 1] === t[j - 1]) {
+      // 比较s[i - 1] 和 t[j - 1]，因为 dp[i][j] 表示前 i 个和前 j 个。
+      if (s[i - 1] === t[j - 1]) { // 代表字符
         // s和t的当前字符相等的时候，可以选择将s和t都前移，也可以选择只前移s，t不前移，个数就是两者相加
-        // 比如s = "rabbbit", t = "rabbit"，当s和t都在第一个b的时候，可以选择s和t都前移，也可以选择只前移s，不前移t
+        // 比如s = "rabbbit", t = "rabbit"，当s和t都在第一个b的时候，可以选择s和t都前移（即当前 b 选择匹配），也可以选择只前移s（s 当前的 b 不选则匹配，跳过它），不前移t（保持 t 的位置，下一个 s 的位置来和它匹配）
         // 如果我不用当前这个 s[i - 1]，那问题就变成：用 s 前 i - 1 个字符，去组成 t 前 j 个字符，有多少种方式？
         // 使用 s[i - 1]：用 s 前 i - 1 个字符，去组成 t 前 j - 1 个字符，有多少种方式
-        // 不用当前字符的方式数 + 使用当前字符的方式数
-        // dp[i - 1][j]：s=abcd 和 t=ac， 在s，t都在c这个位置，用s的c => ab和 a 有多少种方式；不用s的c => ab 和 ac 有多少种方式（而不是ab和a，因为接下来是ab...d 和 ac相比较，b和d相比较）
+        // 不用当前字符的方式数（即 s 的前 i-1 个（现在看的是 i）和 t 的前j个（现在看的是 j）） + 使用当前字符的方式数
         dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1]
       } else {
-        // 当S[j] != T[i]时，那么出现个数不变，dp自然就延续前一个值，即dp[i][j] = dp[i][j-1]
+        // 当S[i] != T[j]时，那么出现个数不变，dp自然就延续前一个值，即dp[i][j] = dp[i-1][j]
         dp[i][j] = dp[i - 1][j]
       }
     }
   }
   return dp[n][m]
 };
-
-
-// 练习
-var numDistinct10 = () => {
-
-}
--- Hard/1235. Maximum Profit in Job Scheduling.js
+++ Hard/1235. Maximum Profit in Job Scheduling.js
@@ -92,3 +92,152 @@ const profit = [50, 10, 40, 70];
 
 const result = jobScheduling(startTime, endTime, profit);
 console.log(result); // 输出最大总利润
+
+// DFS with MEMO (Timeout) O(N^2) 时间复杂度，最坏情况下每个工作都需要递归遍历剩余的工作，使用备忘录避免重复计算
+var jobScheduling = function (startTime, endTime, profit) {
+  let n = startTime.length, jobs = [], memo = new Array(n).fill(-1);
+  for (let i = 0; i < n; i++) { jobs.push([startTime[i], endTime[i], profit[i]]) }
+  jobs.sort((a, b) => a[0] - b[0]);
+
+  // DFS with MEMO (Timeout) 方法的核心思想是对每个工作有选择地进行递归，使用备忘录避免重复计算
+  // 对每个工作，有两种选择：跳过当前工作或选择当前工作
+  // 跳过当前工作：直接递归下一个工作
+  // 选择当前工作：找到下一个与当前工作不冲突的工作，递归计算其最大利润，然后加上当前工作的利润
+  function dfs(i) {
+    if (i >= n) return 0;
+    if (memo[i] !== -1) return memo[i];
+
+    const [start, end, money] = jobs[i];
+    let next = i + 1;
+    while (next < n && jobs[next][0] < end) { next++; }
+
+    const skip = dfs(i + 1); // 跳过当前工作，递归计算下一个工作的最大利润
+    const take = money + dfs(next); // 选择当前工作，递归计算下一个不冲突工作的最大利润
+
+    memo[i] = Math.max(skip, take);
+    return memo[i];
+  }
+
+  return dfs(0);
+};
+
+// DFS with Memo + Binary Search
+var jobScheduling = function (startTime, endTime, profit) {
+  let n = startTime.length, jobs = [], memo = new Array(n).fill(-1);
+  for (let i = 0; i < n; i++) { jobs.push([startTime[i], endTime[i], profit[i]]) }
+  jobs.sort((a, b) => a[0] - b[0]);
+
+  function lowerBound(target) {
+    let left = 0, right = n;
+
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (jobs[mid][0] < target) {
+        left = mid + 1;
+      } else { // jobs[mid][0] >= target
+        right = mid;
+      }
+    }
+
+    return left;
+  }
+
+  function dfs(i) {
+    if (i >= n) return 0;
+    if (memo[i] !== -1) return memo[i];
+
+    const [start, end, money] = jobs[i];
+    // 不选当前 job
+    const skip = dfs(i + 1);
+    // 选当前 job，然后跳到第一个 start >= end 的 job
+    const next = lowerBound(end);
+    const take = money + dfs(next);
+
+    memo[i] = Math.max(skip, take);
+    return memo[i];
+  }
+
+  return dfs(0);
+};
+
+
+// 以上的反转，就是 DP 的方法
+var jobScheduling = function (startTime, endTime, profit) {
+  // dp[i] = 从第 i 个 job 开始能拿到的最大 profit
+  // 多开一个位置，dp[n] = 0，表示没有 job 可以选了
+  const n = startTime.length, jobs = [], dp = new Array(n + 1).fill(0);
+  for (let i = 0; i < n; i++) { jobs.push([startTime[i], endTime[i], profit[i]]) }
+  // 按 start time 排序
+  jobs.sort((a, b) => a[0] - b[0]);
+
+  function lowerBound(target) {
+    let left = 0, right = n;
+
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (jobs[mid][0] >= target) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return left;
+  }
+
+  for (let i = n - 1; i >= 0; i--) { // 每一个相当于后面已经最优了，现在前面多加一个进来，
+    const [start, end, money] = jobs[i];
+    const skip = dp[i + 1]; // 不选当前（当前不 take profit），即选择下一个作为当前
+    const take = money + dp[lowerBound(end)]; // 当前take profit，后面哪一个和它不冲突
+    dp[i] = Math.max(skip, take);
+  }
+
+  return dp[0];
+};
+
+// DP 从前往后的方法（按照 end time 排序），dp[i] 表示以第 i 个 job 作为最后一个选择的最大利润
+var jobScheduling = function (startTime, endTime, profit) {
+  const n = startTime.length;
+  const jobs = [];
+
+  for (let i = 0; i < n; i++) {
+    jobs.push([startTime[i], endTime[i], profit[i]]);
+  }
+
+  // 注意：这里按 end time 排序
+  jobs.sort((a, b) => a[1] - b[1]);
+  const ends = jobs.map(job => job[1]);
+  // dp[i] = 前 i 个 jobs 里面最多能赚多少钱
+  const dp = new Array(n + 1).fill(0);
+  function upperBound(target) {
+    let left = 0, right = n;
+
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (ends[mid] <= target) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+
+    return left;
+  }
+
+  for (let i = 1; i <= n; i++) {
+    const [start, end, money] = jobs[i - 1];
+
+    // 找到有多少个 job 的 end <= 当前 start
+    const prevCount = upperBound(start);
+
+    const skip = dp[i - 1];
+    const take = money + dp[prevCount];
+
+    dp[i] = Math.max(skip, take);
+  }
+
+  return dp[n];
+};
