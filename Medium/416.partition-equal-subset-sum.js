d8875460abc21cb79935c9ea9e5cf1f9b6a05725
Pintaigao He <pintaigao@gmail.com>
2026-07-23 15:11:09 -0500

    wip

--- Medium/416.partition-equal-subset-sum.js
+++ Medium/416.partition-equal-subset-sum.js
@@ -17,8 +17,7 @@ var canPartition = function (nums) {
 
   // 特判：如果是奇数，就不符合要求
   if (sum % 2 !== 0) return false;
-  let target = sum / 2; //目标和,等价于在数组中搜索是否有其中的数等于target
-  let flag = false;
+  let target = sum / 2, flag = false; //目标和,等价于在数组中搜索是否有其中的数等于target
   isTrue(0, 0);
 
   function isTrue(index, sum) {
@@ -48,9 +47,9 @@ var canPartition10 = function (nums) {
   } else {
     sum = sum / 2;
   }
-
+  // 初始化 dp 数组，dp[i][j] 表示前 i 个nums 能否凑出和为 j
   let dp = Array.from({ length: nums.length + 1 }, () => Array(sum + 1).fill(false));
-  // base case
+  // base case,背包没有容量的话，任何物品都可以不放（恰好可以将背包装满0容量），所以 dp[i][0] = true
   for (let i = 0; i <= n; i++) dp[i][0] = true;
 
   // i 表示前 i 个物品，j 表示背包容量 （i 表示 nums 里面的数，j 表示目标和）
@@ -60,7 +59,9 @@ var canPartition10 = function (nums) {
         // 背包容量不足，不能装入第 i 个物品
         dp[i][j] = dp[i - 1][j];
       } else {
-        // 装入或不装入背包
+        // 如果不把 nums[i] 算入子集，或者说你不把这第 i 个物品装入背包，那么是否能够恰好装满背包，取决于上一个状态 dp[i-1][j]，继承之前的结果。
+        // 如果把 nums[i] 算入子集，或者说你把这第 i 个物品装入了背包，那么是否能够恰好装满背包，取决于状态 dp[i - 1][j - nums[i - 1]]。
+        // 不装入 或 装入背包 （选择 true 的那个）
         dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
       }
     }
